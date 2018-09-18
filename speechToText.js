69256bf8b05a23b2d843c9524d3e9781c9bcd029
Luca Strebel <luca.strebel@gmx.net>
2018-09-18 22:50:20 +0200

    added config + speechToText

---
+++ speechToText.js
@@ -0,0 +1,43 @@
+module.exports = {
+    getTextByUrl
+}
+
+const SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
+const fs = require('fs');
+const download = require('download');
+const config = require('./config.json');
+
+var speechToText = new SpeechToTextV1({
+    username: config.username,
+    password: config.password,
+    url: 'https://stream.watsonplatform.net/speech-to-text/api/'
+});
+
+function getTextByUrl(url) {
+    var audioPath = __dirname + './../temp/audio_' + new Date().valueOf() + '.mp3';
+    return new Promise((resolve, reject) => {
+        download(url).then(data => {
+            fs.writeFileSync(audioPath, data);
+        }).then(() => {
+            recognize(audioPath, 'audio/mpeg', './transcription.txt')
+                .then(e => resolve(e))
+                .catch(e => reject(e));
+        });
+    });
+}
+
+function recognize(audio, content_type, dest) {
+    var params = {
+        audio: fs.createReadStream(audio),
+        content_type: content_type
+    };
+
+    return new Promise((resolve, reject) => {
+        speechToText.recognize(params, function (err, res) {
+            if (err)
+                reject(err);
+            else
+                resolve(res.results[0].alternatives[0].transcript);
+        });
+    });
+}
